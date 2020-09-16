ddd56eba3f8a477ebca6516d10f98c1e1ba7174a
ElishaBayode <elisha.are@fuoye.edu.ng>
2020-09-16 14:50:37 +0200

    adding script to read-in SA data

--- data/readReportedCasesSA.R
+++ data/readReportedCasesSA.R
@@ -1,4 +1,7 @@
-location <- "data/reportedCasesSA.csv"
-linkRaw <- "https://github.com/dsfsi/covid19za/blob/master/data/nicd_daily_national_report.csv"
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+this_folder <- "."
+location <- "reportedCasesSA.csv"
+linkRaw <- "https://raw.githubusercontent.com/dsfsi/covid19za/master/data/nicd_daily_national_report.csv"
 data <- readr::read_csv(linkRaw)
 readr::write_csv(data, file.path(this_folder, location))
+
