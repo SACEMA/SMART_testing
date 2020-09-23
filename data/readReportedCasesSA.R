9c7226e27375c566abe0df7bc123412dbf017901
ElishaBayode <elisha.are@fuoye.edu.ng>
2020-09-23 13:57:02 +0200

    now set for plotting

--- data/readReportedCasesSA.R
+++ data/readReportedCasesSA.R
@@ -9,7 +9,7 @@ linkRaw <- "https://raw.githubusercontent.com/dsfsi/covid19za/master/data/nicd_d
 data <- readr::read_csv(linkRaw)
 readr::write_csv(data, file.path(this_folder, location))
 
-NICD_caseReport <- data %>%
+NICD_caseReport <- as.data.frame(data) %>%
 mutate(totalCases = male_total_cases + unknown_sex_total_cases + female_total_cases) %>% 
 mutate(DailyPos = (totalCases - lag(totalCases, default = 0))) %>%
 mutate(DailyTestsCat_1 = (testing_passive - lag(testing_passive, default = 0))+
@@ -25,6 +25,8 @@ mutate(communityTesting = (testing_community_screening - lag(testing_community_s
 mutate(passiveTesting = (testing_passive - lag(testing_passive, default = 0))) %>%
 mutate(positivityRate= DailyPos/DailyTestsCat_1)%>%
 mutate(Date = as.Date(date, format = "%d-%m-%Y")) 
+
+NICD_caseReport <- NICD_caseReport[-1,]
 
 
 
